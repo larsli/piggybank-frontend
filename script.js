4f87dfb81c65ff71c0d50f78a860ce71e8b8b13e
Lars Lind <lars.lind@mossww.com>
2025-12-25 12:27:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const BASE_URL = "https://piggybank-backend.onrender.com";
+async function updateBalance(child) {
+    const res = await fetch(`${https://piggybank-backend-kidl.onrender.com}/balance/${child}`);
+    const data = await res.json();
+
+    if (child === "child1")
+        document.getElementById("balance1").innerText = data.balance;
+    else
+        document.getElementById("balance2").innerText = data.balance;
+}
+
+async function deposit(child) {
+    const amount = Number(document.getElementById(child === "child1" ? "amount1" : "amount2").value);
+
+    await fetch(`${BASE_URL}/deposit/${child}`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({amount})
+    });
+
+    updateBalance(child);
+}
+
+async function withdraw(child) {
+    const amount = Number(document.getElementById(child === "child1" ? "amount1" : "amount2").value);
+
+    await fetch(`${BASE_URL}/withdraw/${child}`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({amount})
+    });
+
+    updateBalance(child);
+}
+
+updateBalance("child1");
+updateBalance("child2");
